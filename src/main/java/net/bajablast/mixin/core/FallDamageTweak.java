64bd464d99f5a5cf56f3d983dadcab4def012eea
BajaBlast-US <kidsalex13@gmail.com>
2026-02-24 04:30:17 -0600

    Started setting up mixin tweak files, and removed examples

---
+++ src/main/java/net/bajablast/mixin/core/FallDamageTweak.java
@@ -0,0 +1,9 @@
+package net.bajablast.mixin.core;
+
+import net.minecraft.data.tags.VanillaBlockTagsProvider;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(VanillaBlockTagsProvider.class)
+public abstract class FallDamageTweak {
+
+}
---
+++ src/main/java/net/bajablast/mixin/core/GameruleTweaks.java
@@ -0,0 +1,4 @@
+package net.bajablast.mixin.core;
+
+public class GameruleTweaks {
+}
