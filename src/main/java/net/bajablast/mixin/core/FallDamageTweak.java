38607198bd3b84a59a577bd5736396ea58189479
BajaBlast-US <kidsalex13@gmail.com>
2026-02-26 12:05:45 -0600

    gave up on gamerules, doing the blocks now

--- src/main/java/net/bajablast/mixin/core/FallDamageTweak.java
+++
@@ -1,9 +0,0 @@
-package net.bajablast.mixin.core;
-
-import net.minecraft.data.tags.VanillaBlockTagsProvider;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(VanillaBlockTagsProvider.class)
-public abstract class FallDamageTweak {
-
-}
--- src/main/java/net/bajablast/mixin/core/GameRuleTweaks.java
+++ src/main/java/net/bajablast/mixin/core/GameRuleTweaks.java
@@ -12,17 +12,5 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameRules.class)
 public class GameRuleTweaks extends JavaPlugin {
-
-  @Inject(method = "<init>*", at = @At("TAIL"))
-  private void forceKeepInventory(CallbackInfo ci) {
-    ServerLevel level = (ServerLevel) (Object) this;
-
-    GameRules rules =
-      ((PrimaryLevelData) level.getLevelData())
-        .getGameRules();
-
-    rules.getRule(GameRules.RULE_KEEPINVENTORY)
-      .set(true, level);
-  }
 }
 
